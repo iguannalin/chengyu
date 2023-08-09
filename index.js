7a627b959c4a8bf1a38260c4462e3748088f9fb4
seasons986 <annalin12345@gmail.com>
2023-08-09 01:38:37 -0400

    gasp new toy

--- index.js
+++ index.js
@@ -4,4 +4,40 @@ window.addEventListener("load", () => {
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min) + min); // The maximum is exclusive and the minimum is inclusive
   }
+
+  let div = document.getElementById("poem");
+  let phrases = [];
+  let foundPhrases = [];
+
+  // amazing chengyu data source -- http://thuocl.thunlp.org/
+  fetch("THUOCL_chengyu.txt").then((f) => f.text()).then((r) => {
+    phrases = r.split(",");
+    getRandomPhrase();
+  });
+  
+  function showPhrase(phrase) { // hehe
+    if (!phrase) return;
+    phrase.split("").forEach((letter, index) => {
+      let span = document.createElement("a");
+      span.innerHTML = letter;
+      if (index == (phrase.length - 1)) span.innerHTML += "  ";
+      span.classList = "grayed";
+      span.onclick = connectWord;
+      div.appendChild(span);
+    });
+  }
+
+  function connectWord(e) {
+    e.target.onclick = null;
+    e.target.classList.remove("grayed");
+    let target = e.target.innerText;
+    let found = phrases.find((v) => v.startsWith(target) && !foundPhrases.includes(v));
+    showPhrase(found);
+  }
+
+  function getRandomPhrase() {
+    let phrase = phrases[getRandomInt(0, phrases.length)];
+    foundPhrases.push(phrase);
+    showPhrase(phrase);
+  }
 });
